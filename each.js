2e740ea3c91cb0bfb4c831bbab86a0dd85876ead
Rachit Chaudhary <rachit.chaudhary.17@mountblue.tech>
2021-08-14 11:00:57 +0530

    Update "each.js" with commented out problem statement

--- each.js
+++ each.js
@@ -1,3 +1,12 @@
+// function each(elements, cb) {
+    // Do NOT use forEach to complete this function.
+    // Iterates over a list of elements, yielding each in turn to the `cb` function.
+    // This only needs to work with arrays.
+    // You should also pass the index into `cb` as the second argument
+    // based off http://underscorejs.org/#each
+// }
+
+
 module.exports = {
     each : (elements, cb) => {
         if(elements instanceof Array){
